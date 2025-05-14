253e8e33a3197be91aa6bbde106fa04824e68156
ISTdatateam <istdatateam@gmail.com>
2025-05-14 17:56:00 -0400

    fix page 04

--- src/forms/pages/04_declaraciones_fotos.py
+++ src/forms/pages/04_declaraciones_fotos.py
@@ -12,72 +12,71 @@ def run():
     with st.expander("Debug"):
         st.write(st.session_state)
 
-    with st.form(key="form_declaraciones"):
-        # Declaración del accidentado
-        st.text_area(
-            'Declaración Accidentado/a*',
-            value=st.session_state.declaracion_accidentado,
-            height=150,
-            key='declaracion_accidentado'
-        )
-        st.divider()
 
-        # Testigo 1
-        st.write("**Testigo 1**")
-        st.text_input(
-            'Nombre Testigo 1*',
-            value=st.session_state.decl1_nombre,
-            key='decl1_nombre'
-        )
-        st.text_input(
-            'Cargo Testigo 1*',
-            value=st.session_state.decl1_cargo,
-            key='decl1_cargo'
-        )
-        st.text_input(
-            'RUT Testigo 1*',
-            value=st.session_state.decl1_rut,
-            key='decl1_rut'
-        )
-        st.text_area(
-            'Texto Declaración 1*',
-            value=st.session_state.decl1_texto,
-            key='decl1_texto'
-        )
-        st.divider()
+    # Declaración del accidentado
+    st.session_state.declaracion_accidentado = st.text_area(
+        "Declaración Accidentado/a*",
+        st.session_state.get('declaracion_accidentado', ''),
+        height=150
+    )
+    st.divider()
 
-        # Testigo 2
-        st.write("**Testigo 2**")
-        st.text_input(
-            'Nombre Testigo 2*',
-            value=st.session_state.decl2_nombre,
-            key='decl2_nombre'
-        )
-        st.text_input(
-            'Cargo Testigo 2*',
-            value=st.session_state.decl2_cargo,
-            key='decl2_cargo'
-        )
-        st.text_input(
-            'RUT Testigo 2*',
-            value=st.session_state.decl2_rut,
-            key='decl2_rut'
-        )
-        st.text_area(
-            'Texto Declaración 2*',
-            value=st.session_state.decl2_texto,
-            key='decl2_texto'
-        )
+    # Testigo 1
+    st.write("**Testigo 1**")
+    st.session_state.decl1_nombre = st.text_input(
+        "Nombre Testigo 1*",
+        st.session_state.get('decl1_nombre', ''),
+        help="Ej: Indica el nombre con dos apellidos"
+    )
+    st.session_state.decl1_cargo = st.text_input(
+        "Cargo Testigo 1*",
+        st.session_state.get('decl1_cargo', ''),
+        help="Ej: Indica el cargo del testigo"
+    )
+    st.session_state.decl1_rut = st.text_input(
+        "RUT Testigo 1*",
+        st.session_state.get('decl1_rut', ''),
+        help="Ej: Indica el rut del testigo"
+    )
+    st.session_state.decl1_texto = st.text_area(
+        "Texto Declaración 1*",
+        st.session_state.get('decl1_texto', ''),
+    )
 
-        # Fotos del accidente
-        st.file_uploader(
-            'Fotos del Lugar',
-            type=['png', 'jpg', 'jpeg'],
-            accept_multiple_files=True,
-            key='fotos_accidente'
-        )
+    st.divider()
 
-        # Botón Siguiente
-        if st.form_submit_button('Siguiente ▶'):
-            #st.session_state['_page'] = 5
-            st.rerun()
+    # Testigo 2
+    st.write("**Testigo 2**")
+    st.session_state.decl2_nombre = st.text_input(
+        "Nombre Testigo 2*",
+        st.session_state.get('decl2_nombre', ''),
+        help="Ej: Indica el nombre con dos apellidos"
+    )
+    st.session_state.decl2_cargo = st.text_input(
+        "Cargo Testigo 2*",
+        st.session_state.get('decl2_cargo', ''),
+        help="Ej: Indica el cargo del testigo"
+    )
+    st.session_state.decl2_rut = st.text_input(
+        "RUT Testigo 2*",
+        st.session_state.get('decl2_rut', ''),
+        help="Ej: Indica el rut del testigo"
+    )
+    st.session_state.decl2_texto = st.text_area(
+        "Texto Declaración 2*",
+        st.session_state.get('decl2_texto', ''),
+    )
+
+    # Fotos del accidente
+    st.file_uploader(
+        'Fotos del Lugar',
+        type=['png', 'jpg', 'jpeg'],
+        accept_multiple_files=True,
+        key='fotos_accidente'
+    )
+
+    # Botón Siguiente
+    if st.button('Siguiente ▶'):
+        st.success("Sección Declaraciones guardada")
+        #st.session_state['_page'] = 5
+        st.rerun()
