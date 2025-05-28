9a282c11d9a629596440b27897caff28a10ac93e
ISTdatateam <istdatateam@gmail.com>
2025-05-28 17:38:04 -0400

    David - 202505201647

--- src/report/generator.py
+++ src/report/generator.py
@@ -17,7 +17,6 @@ class InformeGenerator:
         """
         Genera una imagen PNG a partir del string DOT y devuelve los bytes.
         """
-        dot_source = dot_source.replace('{', '{\n    graph [dpi=300];', 1)
         graph = Source(dot_source, format='png')
         return graph.pipe()
 
